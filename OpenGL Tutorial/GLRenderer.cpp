9bc7c28b3a1c731807c07bff9fd2df5474ccbeb4
James Tasker <james_tasker1@hotmail.com>
2012-07-30 22:15:44 +0100

    Add depth testing and second polygon
    Signed-off-by: James Tasker <james_tasker1@hotmail.com>

--- OpenGL Tutorial/GLRenderer.cpp
+++ OpenGL Tutorial/GLRenderer.cpp
@@ -16,6 +16,9 @@ int GLRenderer::Initialise(HDC hdc, unsigned int width, unsigned int height)
 
 	//Use the defined window dimensions
 	Resize(width, height);
+	
+	//Turn on depth testing
+	glEnable(GL_DEPTH_TEST);
 
 	//Set the default cleared buffer colour
 	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
@@ -61,7 +64,7 @@ void GLRenderer::Resize(unsigned int width, unsigned int height)
 void GLRenderer::Render()
 {
 	//Clear the colour buffer
-	glClear(GL_COLOR_BUFFER_BIT);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	//Use the identity modelview matrix
 	glLoadIdentity();
@@ -89,6 +92,23 @@ void GLRenderer::Render()
 		glVertex3f(1.0f, -1.0f, 0.0f);	//Create the bottom right
 
 	//Stop drawing triangles
+	glEnd();
+
+	//Start drawing quadrangles
+	glBegin(GL_QUADS);
+
+	  glColor3f(0.5f, 0.5f, 0.5f);		//Set the colour to dark grey
+	  glVertex3f(-1.0f, -1.0f, 0.0f);	//Create the bottom left vertex
+
+	  glColor3f(0.5f, 0.5f, 0.5f);		//Set the colour to dark grey
+	  glVertex3f(-0.8f, 1.0f, 0.0f);	//Create the top left vertex
+
+	  glColor3f(0.75f, 0.75f, 0.75f);	//Set the colour to light grey
+	  glVertex3f(1.0f, 0.5f, -1.0f);	//Create the top right vertex
+
+	  glColor3f(0.75f, 0.75f, 0.75f);	//Set the colour to light grey
+	  glVertex3f(0.6f, -0.9f, -1.0f);	//Create the bottom right vertex
+
 	glEnd();
 
 	//Display the backbuffer
